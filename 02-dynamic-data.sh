05bbd2736bae20a232e58cd96aba996a79220905
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-10 21:48:08 -0800

    dynamic data

--- 02-dynamic-data.sh
+++ 02-dynamic-data.sh
@@ -6,7 +6,7 @@ b=200
 echo -e "\e[32m First Value is $a\e[0m"
 echo -e "\e[33m Second Value is $b\e[0m"
 
-add = $((a + b))"
+add = "$((a + b))"
 
 echo -e "\e[34m Total of above two values is $add"
 
