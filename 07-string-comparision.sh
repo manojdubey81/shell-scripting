bb22c6f2f72c8635f9b3c6bf6aa60ecda6e2a761
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 10:45:03 -0800

    string comparision

--- 07-string-comparision.sh
+++ 07-string-comparision.sh
@@ -3,9 +3,9 @@
 # Sting '=' comparision
 read -p "Enter your string: " str
 if [ "$str" = "ABC" ]; then
-  echo -e "\e[32mboth strings are equal\e[0m"
+  echo -e "\e[32min '=' comparision strings are equal\e[0m"
 else
-  echo -e "\e[31mboth strings are not equal\e[0m"
+  echo -e "\e[31min '=' comparision both strings are not equal\e[0m"
 fi
 
 
@@ -13,7 +13,7 @@ fi
 #string '==' comparision
 
 if [ "str" == "ABC" ]; then
-  echo -e "\e[33mboth string are not equal\e[0m"
+  echo -e "\e[33min '==' comparision both string are not equal\e[0m"
 else
-  echo -e "\e[34mboth string are equal\e[0m"
+  echo -e "\e[34min '==' comparision both string are equal\e[0m"
 fi
