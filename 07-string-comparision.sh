9f88027071b362151b2191f7d96de3781e4f4e53
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 10:52:38 -0800

    string comparision

--- 07-string-comparision.sh
+++ 07-string-comparision.sh
@@ -21,7 +21,7 @@ fi
 
 #string '-z' test
 
-if [ -z $str]; then
+if [ ! -z $str ]; then
   echo -e "\e[36minput string holds some value\e[0m"
 else
   echo -e "\e[35minput string does not hold any value\e[0m"
