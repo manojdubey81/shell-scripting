06412e61bf3bf7017b1f2ce8cfffe8f0f50a8333
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 11:39:02 -0800

    file comparision

--- 09-file-comparision.sh
+++ 09-file-comparision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -e "Enter your string: " str
+read -p "Enter your string: " str
 
-echo "first char of string is : " $str[0]
-echo "second char of string is : " $str[1]
+echo "first char of string is : " ${str[0]}
+echo "second char of string is : " ${}str[1]""
