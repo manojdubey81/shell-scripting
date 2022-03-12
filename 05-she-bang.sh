f718e6231b6a881206d06574b48079310353db0e
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-11 22:07:05 -0800

    she-bang script base

--- 05-she-bang.sh
+++ 05-she-bang.sh
@@ -5,8 +5,8 @@ echo -e "\e[32m \n Welcome Shyam and Tapas \e[0m"
 echo -e "\e[33m \n Welcome Shyam and Tapas \e[0m"
 echo -e "\e[34m \n Welcome Shyam and \t Tapas \e[0m"
 echo -e "\e[35m \n Welcome Shyam and \t Tapas \e[0m"
-echo -e "\e[36m \n Welcome Shyam and \t Tapas \e[0m"
+echo -e "\e[36m \n Welcome Shyam and \t Tapas \n\e[0m"
 
-read -p "\e[31m Enter Your Name: \e[0m" name
-echo "\e[33m Your Name is: \e[0m" $name
+read -p '\e[31m Enter Your Name: \e[0m' name
+echo '\e[33m Your Name is: \e[0m' $name
 
