f3b62a9c0656f675796514de60b8775e9801d016
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 10:04:59 -0800

    Dunamic doller args

--- 04-dynamic-args$-values.sh
+++ 04-dynamic-args$-values.sh
@@ -3,5 +3,6 @@
 echo -e "\e[33m Script name is : \e[0m $0"
 echo -e "\e[33m First Args is : \e[0m $1"
 echo -e "\e[33m Second Args is : \e[0m $2"
-echo -e "\e[33m Total Args are : \e[0m $*"
+echo -e "\e[33m All Args are : \e[0m $*"
+echo -e "\e[33m All Args are : \e[0m $@"
 echo -e "\e[33m No of Args are : \e[0m $#"
