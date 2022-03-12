4c67040a867bd7000c94888e354cec31eb8e0e81
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-11 21:56:40 -0800

    she-bang script base

--- 05-she-bang.sh
+++ 05-she-bang.sh
@@ -1,4 +1,9 @@
 #!bin/bash
 
-echo Thansk Tapas to Join
+echo -e "\e[31m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[32m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[33m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[34m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[35m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[36m Welcome Shyam and Tapas \e[0m"
 
