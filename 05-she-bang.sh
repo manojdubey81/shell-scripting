81e323cd5f406d99e96299209aeeeba1b93d2676
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-11 21:59:34 -0800

    she-bang script base

--- 05-she-bang.sh
+++ 05-she-bang.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-echo -e "\e[31m Welcome Shyam and Tapas \e[0m"
-echo -e "\e[32m Welcome Shyam and Tapas \e[0m"
-echo -e "\e[33m Welcome Shyam and Tapas \e[0m"
-echo -e "\e[34m Welcome Shyam and Tapas \e[0m"
-echo -e "\e[35m Welcome Shyam and Tapas \e[0m"
-echo -e "\e[36m Welcome Shyam and Tapas \e[0m"
+echo -e "\e[31m \n Welcome Shyam and Tapas \e[0m"
+echo -e "\e[32m \n Welcome Shyam and Tapas \e[0m"
+echo -e "\e[33m \n Welcome Shyam and Tapas \e[0m"
+echo -e "\e[34m \n Welcome Shyam and \t Tapas \e[0m"
+echo -e "\e[35m \n Welcome Shyam and \t Tapas \e[0m"
+echo -e "\e[36m \n Welcome Shyam and \t Tapas \e[0m"
 
