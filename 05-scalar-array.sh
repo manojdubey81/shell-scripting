50e6229b4b0bca8dac5d83157be93b71c9e48c8b
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 09:27:35 -0800

    array and scalar values

--- 05-scalar-array.sh
+++ 05-scalar-array.sh
@@ -5,4 +5,4 @@ a = (10 20 30 manoj 'I am ok')
 echo -e "\e[32m Arrays First element is : \e[0m" ${a(0)}
 echo -e "\n \e[32m Arrays Second element is : \e[0m" ${a(1)}
 echo -e "\n \e[32m Arrays Third element is : \e[0m" ${a(2)}
-echo -e "\n \e[32m Arrays Forth element is : \e[0m" $a
+#echo -e "\n \e[32m Arrays Forth element is : \e[0m" $a
