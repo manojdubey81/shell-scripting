27633ff9e10a9af8ca799bd48d0e5035428a8731
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 11:20:59 -0800

    numeric comparision

--- 08-numeric-comparision.sh
+++ 08-numeric-comparision.sh
@@ -3,9 +3,9 @@
 # -eq numeric comparision
 read -p "Type your input: " num
 
-numval='^[0-9]+$'
+numval=[0 1 2 3 4 5 6 7 8 9]
 if [ $num -ne  $numval ]; then
   echo -e "\e[33mYou entered a number\e[0m"
 else
-  echo -e "\e[31mYou entered a string"
+  echo -e "\e[31mYou entered a string\e[0m"
 fi
