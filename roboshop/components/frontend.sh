2bafb5fc94908702b8870579044bd368a9ba17c3
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-11 20:20:32 -0800

    updating frontend script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+Statcheck () {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m- SUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+}
+
+Print() {
+  echo -e "\e[36m $1 \e[0m"
+}
 
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
@@ -6,45 +18,28 @@ if [ "$USER_ID" -ne 0 ]; then
   exit 1
 fi
 
-echo -e "\e[36m Installing Nginx\e[0m"
+Print "Installing Nginx"
 yum install nginx -y
+Statcheck
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
-
-
-echo -e "\e[36m Download nginx content\e[0m"
+Print "Download nginx content"
 
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Statcheck
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
 
-echo -e "\e[36m Cleanup Old Nginx Content and Extract new downloaded archive \e[0m"
+Print "Cleanup Old Nginx Content and Extract new downloaded archive"
 
 cd /usr/share/nginx/html/
+
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Statcheck
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
-
-echo -e "\e[36m Restart Nginx\e[0m"
+Print "Restart Nginx"
 systemctl restart nginx
 systemctl enable nginx
