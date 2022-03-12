dfbdf70f106b60827b8e6c84e5b5269e33d42560
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-12 10:41:39 -0800

    string comparision

--- 07-string-comparision.sh
+++ 07-string-comparision.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-# Sting '=' comparision for not equal condition
+# Sting '=' comparision
 read -p "Enter your string: " str
 if [ "$str" = "ABC" ]; then
   echo -e "\e[32mboth strings are equal\e[0m"
@@ -8,3 +8,12 @@ else
   echo -e "\e[31mboth strings are not equal\e[0m"
 fi
 
+
+
+#string '==' comparision
+
+if [ "str" == "ABC"]; then
+  echo -e "\e[33mboth string are not equal\e[0m"
+else
+  echo -e "\e[34mboth string are equal\e[0m"
+fi
