ecee32a5eb2f0398e3201e985076496be70cc8d8
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-14 13:18:40 -0700

    frontend updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -16,21 +16,15 @@ Print "Cleanup Old Nginx Content"
 rm -rf /usr/share/nginx/html/* &>>LOG_FILE
 Statcheck $? "Old Nginx Content Cleanup"
 
-Print "Switch to nginx"
 cd /usr/share/nginx/html/
-Statcheck $?
-
-Print "Cleanup Old Nginx"
-rm -rf /tmp/frontend.zip &>>LOG_FILE
-Statcheck $? "Old Nginx Cleanup"
 
 
 Print "Extract configuration"
-unzip -o /tmp/frontend.zip && mv frontend-main/* . && mv static/* . &>>LOG_FILE
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . &>>LOG_FILE
 Statcheck $? "Configuration Extraction"
 
 Print "Update RoboShop Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>>LOG_FILE
 Statcheck $? "RoboShop Configuration Updated"
 
 Print "Start Nginx"
