6fbc4db75b4de189406b836c0d7936745808781a
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-14 13:25:57 -0700

    frontend updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,28 +4,27 @@ source components/common.sh
 
 Print "Installing Nginx"
 yum install nginx -y &>>LOG_FILE
-Statcheck $?
+Statcheck $? "Nginx Installed-"
 
-Print "Download nginx content"
-
+Print "Extract Nginx Repo"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
-Statcheck $? "Installing Nginx"
+Statcheck $? "Nginx Extraction-"
 
 
-Print "Cleanup Old Nginx Content"
+Print "Cleanup Old Nginx"
 rm -rf /usr/share/nginx/html/* &>>LOG_FILE
-Statcheck $? "Old Nginx Content Cleanup"
+Statcheck $? "Old Nginx Cleanup-"
 
 cd /usr/share/nginx/html/
 
 
 Print "Extract configuration"
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . &>>LOG_FILE
-Statcheck $? "Configuration Extraction"
+Statcheck $? "Configuration Extraction-"
 
-Print "Update RoboShop Configuration"
+Print "RoboShop Configuration Update"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>>LOG_FILE
-Statcheck $? "RoboShop Configuration Updated"
+Statcheck $? "RoboShop Configuration Updated-"
 
 Print "Start Nginx"
 systemctl restart nginx && systemctl enable nginx &>>LOG_FILE
