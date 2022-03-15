63588ab6051b19487ca1cc2fc7941e66cc55c9c4
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-15 11:48:29 -0700

    updated frontend_new.sh

--- roboshop/components/mongodb_new.sh
+++ roboshop/components/mongodb_new.sh
@@ -11,7 +11,7 @@ yum install -y mongodb-org &>>${LOG_FILE}
 StatusChk $? "MongoDB Installation"
 
 Print "Update Listen IP"
-sed -i -e 's/127.0.0.1/0.0.0./' /etc/mongod.conf &>>${LOG_FILE}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG_FILE}
 StatusChk $? "Listner IP Update"
 
 Print "Start mongoDB"
