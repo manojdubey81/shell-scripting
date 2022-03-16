425293bf49bdbf737ec60d783f2a67cd271461ca
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-15 21:24:15 -0700

    updated makefile

--- roboshop/components/catalogue_new.sh
+++ roboshop/components/catalogue_new.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 
 source components/common_new.sh
-
-Print "Configure Yum repos"
-  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>"${LOG_FILE}"
-StatusChk $? "NodeJS Repo Extraction"
-
-Print "Install NodeJS"
-  yum install nodejs gcc-c++ -y &>>"${LOG_FILE}"
-StatusChk $? "NodeJS Install"
-
-Print "Add Deamon user"
-id roboshop  &>>"${LOG_FILE}"
-if [ $? -ne 0 ]; then
-  Print "Add Application User"
-  useradd roboshop &>>"${LOG_FILE}"
-  StatusChk $? "Deamon Useradd"
-fi
-
-Print "Download catalogue archive"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>"${LOG_FILE}"
-StatusChk $? "catalogue archive downloaded"
-
-Print "Extract and Load catalogue repo"
-cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>"${LOG_FILE}" && mv -o catalogue-main catalogue &>>"${LOG_FILE}"
-StatusChk $? "Extraction of catalogue"
-
-Print "npm Install"
-cd /home/roboshop/catalogue && npm install &>>"${LOG_FILE}"
-StatusChk $? "npm Install"
-
-Print "Update mongodb private IP in SystemD file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>"${LOG_FILE}"
-StatusChk $? "systemd file moved to default location"
-
-sed -i -e '/MONGO_DNSNAME/mongodb_private_ip/' /etc/systemd/system/catalogue.service &>>"${LOG_FILE}"
-StatusChk $? "MONGO_DNSNAME Updated"
-
-Print "Restart deamon"
-systemctl daemon-reload &>>"${LOG_FILE}" && systemctl restart catalogue &>>"${LOG_FILE}" && systemctl enable catalogue &>>"${LOG_FILE}"
-
-echo -e "\e[32m Catalogue Ready to Use\e[0m"
+COMPONENT=catalogue
+NodeJS
--- roboshop/components/common_new.sh
+++ roboshop/components/common_new.sh
@@ -5,7 +5,7 @@ if [ "$USER_ID" -ne 0 ]; then
 fi
 
 StatusChk () {
-  if [ $1 -eq 0 ]; then
+  if [ "$1" -eq 0 ]; then
     echo -e "\e[32m$2-SUCCESS\e[0m"
   else
     echo -e "\e[31m$2-FAILURE\e[0m"
@@ -20,3 +20,47 @@ Print() {
 
 LOG_FILE=/tmp/roboshop.log
 rm -f ${LOG_FILE}
+
+APP_USER=roboshop
+
+NodeJS() {
+  Print "Configure Yum repos"
+    curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>"${LOG_FILE}"
+  StatusChk $? "NodeJS Repo Extraction"
+
+  Print "Install NodeJS"
+    yum install nodejs gcc-c++ -y &>>"${LOG_FILE}"
+  StatusChk $? "NodeJS Install"
+
+  Print "Add Deamon user"
+  id "${APP_USER}"  &>>"${LOG_FILE}"
+  if [ "$?" -ne 0 ]; then
+    Print "Add Application User"
+    useradd "${APP_USER}" &>>"${LOG_FILE}"
+    StatusChk $? "Deamon Useradd"
+  fi
+
+  Print "Download ${COMPONENT} archive"
+  curl -f -s -L -o /tmp/"${COMPONENT}".zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>"${LOG_FILE}"
+  StatusChk $? "${COMPONENT} archive downloaded"
+
+  Print "Extract and Load ${COMPONENT} repo"
+  cd /home/"${APP_USER}" && unzip -o /tmp/"${COMPONENT}".zip &>>"${LOG_FILE}" && mv -o "${COMPONENT}"-main "${COMPONENT}" &>>"${LOG_FILE}"
+  StatusChk $? "Extraction of ${COMPONENT}"
+
+  Print "npm Install"
+  cd /home/"${APP_USER}"/"${COMPONENT}" && npm install &>>"${LOG_FILE}"
+  StatusChk $? "npm Install"
+
+  Print "Update mongodb private IP in SystemD file"
+  mv /home/"${APP_USER}"/"${COMPONENT}"/systemd.service /etc/systemd/system/"${COMPONENT}".service &>>"${LOG_FILE}"
+  StatusChk $? "systemd file moved to default location"
+
+  sed -i -e '/MONGO_DNSNAME/mongodb_private_ip/' /etc/systemd/system/"${COMPONENT}".service &>>"${LOG_FILE}"
+  StatusChk $? "MONGO_DNSNAME Updated"
+
+  Print "Restart deamon"
+  systemctl daemon-reload &>>"${LOG_FILE}" && systemctl restart "${COMPONENT}" &>>"${LOG_FILE}" && systemctl enable "${COMPONENT}" &>>"${LOG_FILE}"
+
+  echo -e "\e[32m ${COMPONENT} Ready to Use\e[0m"
+}
