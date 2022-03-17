b38405a59cb6cd6036aca4384ebc9ee31fa0156b
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-16 22:09:50 -0700

    Update redis_new

---
+++ roboshop/components/redis_new.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source components/common_new.sh
+
+Print "Download Yum Repos"
+curl -f -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+StatusChk $? "Repos downloaded"
+
+Print "Install Redis"
+yum install redis -y &>>${LOG_FILE}
+
+
+Print "Update Redis Config"
+if [ -f /etc/redis.conf ]; then
+  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG_FILE}
+fi
+
+if [ -f /etc/redis/redis.conf ]; then
+  sed -i -e 's/127.0.0.1/0.0.0.0/' //etc/redis/redis.conf &>>${LOG_FILE}
+fi
+SatusChk $? "Redis Config Update"
+
+
+Print "Start Redis Database"
+systemctl enable redis &>>${LOG_FILE} &&  systemctl restart redis &>>${LOG_FILE}
+StatusChk $? "Redis Started"
