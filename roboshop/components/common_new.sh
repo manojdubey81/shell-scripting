d16d2c11b665d37fe3f134beb6c47d90d618f0a1
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-16 21:46:51 -0700

    Update catalogue_new, cart_new, user_new

--- roboshop/components/common_new.sh
+++ roboshop/components/common_new.sh
@@ -23,62 +23,73 @@ rm -f ${LOG_FILE}
 
 APP_USER=roboshop
 
-NodeJS() {
+APP_SETUP() {
 
-  Print "Configure Yum repos"
-    curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
-  StatusChk $? "NodeJS Repo Extraction"
+    Print "Add Deamon user"
+    id ${APP_USER}  &>>${LOG_FILE}
+    if [ $? -ne 0 ]; then
+      Print "Add Application User"
+      useradd ${APP_USER} &>>${LOG_FILE}
+      StatusChk $? "Useradd"
+    fi
 
-  Print "Install NodeJS"
-    yum install nodejs gcc-c++ -y &>>${LOG_FILE}
-  StatusChk $? "NodeJS Install"
+    Print "Download ${COMPONENT} archive"
+    curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>"${LOG_FILE}"
+    StatusChk $? "${COMPONENT} archive downloaded"
 
-  Print "Add Deamon user"
-  id ${APP_USER}  &>>${LOG_FILE}
-  if [ $? -ne 0 ]; then
-    Print "Add Application User"
-    useradd ${APP_USER} &>>${LOG_FILE}
-    StatusChk $? "Useradd"
-  fi
+    Print "CleanUp Old Content"
+    rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
+    StatusChk $? "CleanUp Old ${COMPONENT}"
 
-  Print "Download ${COMPONENT} archive"
-  curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>"${LOG_FILE}"
-  StatusChk $? "${COMPONENT} archive downloaded"
+    Print "Extract and Load ${COMPONENT} content"
+    cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
+    StatusChk $? "Extraction of ${COMPONENT}"
+}
 
-  Print "CleanUp Old Content"
-  rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
-  StatusChk $? "CleanUp Old ${COMPONENT}"
+SERVICE_SETUP() {
 
-  Print "Extract and Load ${COMPONENT} content"
-  cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
-  StatusChk $? "Extraction of ${COMPONENT}"
+    Print "Fix App User Permissions"
+    chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+    StatusChk $? "Permissions Setup"
 
-  Print "Install App Dependencies"
-  cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
-  StatusChk $? "App Dependencies Install"
-
-  Print "Fix App User Permissions"
-  chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
-  StatusChk $? "Permissions Setup"
-
-  Print "Setup SystemD file"
-      sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-              -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-              -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-              -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-              -e 's/CARTENDPOINT/cart.roboshop.internal/' \
-              -e 's/DBHOST/mysql.roboshop.internal/' \
-              -e 's/CARTHOST/cart.roboshop.internal/' \
-              -e 's/USERHOST/user.roboshop.internal/' \
-              -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+    Print "Setup SystemD file"
+    sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+            -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
+            -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
+            -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
+            -e 's/CARTENDPOINT/cart.roboshop.internal/' \
+            -e 's/DBHOST/mysql.roboshop.internal/' \
+            -e 's/CARTHOST/cart.roboshop.internal/' \
+            -e 's/USERHOST/user.roboshop.internal/' \
+            -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
             /home/${APP_USER}/${COMPONENT}/systemd.service &>>${LOG_FILE} && \
             mv /home/${APP_USER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
-  StatusChk $? "MONGO_DNSNAME Updated"
+    StatusChk $? "${COMPONENT} dependencies Updated"
 
-  Print "Restart ${COMPONENT} Service"
-  systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
-  StatusChk $? "${COMPONENT} Service Restart"
+    Print "Restart ${COMPONENT} Service"
+    systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
+    StatusChk $? "${COMPONENT} Service Restart"
+}
 
-  echo -e "\n------------${COMPONENT}--------------"
-  echo -e "\e[32m ${COMPONENT} Ready to Use\e[0m"
+
+NodeJS() {
+
+    Print "Configure Yum repos"
+    curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+    StatusChk $? "NodeJS Repo Extraction"
+
+    Print "Install NodeJS"
+    yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+    StatusChk $? "NodeJS Install"
+
+    APP_SETUP
+
+    Print "Install App Dependencies"
+    cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+    StatusChk $? "App Dependencies Install"
+
+    SERVICE_SETUP
+
+    echo -e "\n------------${COMPONENT}--------------"
+    echo -e "\e[32m ${COMPONENT} Ready to Use\e[0m"
 }
---
+++ roboshop/components/user_new.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source components/common_new.sh
+COMPONENT=user
+NodeJS
