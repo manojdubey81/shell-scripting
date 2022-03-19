763f3d7448b9e9af73cbf5ee1e06e06add305db9
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-18 21:18:07 -0700

    ec2-instance base script

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,19 +3,26 @@
 if [ -z "$1" ]; then
   echo -e '\e[34mMachine Name Required\e[0m'
   exit 1
+elif [ -z "$2" ] th en
+  echo -e '\e[34mInstance type must needed\e[0m'
+  exit 2
 fi
 
+
 COMPONENT="$1"
+INST_TYPE="$2"
 
 SG_ID=$(aws ec2 describe-security-groups \
         --filters Name=group-name,Values=allow-all-sgp \
           | jq '.SecurityGroups[].GroupId' \
           | sed -e 's/"//g')
 
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" \
+        | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 aws ec2 run-instances \
-      --image-id ami-0bb6af715826253bf \
-      --instance-type t2.micro \
+      --image-id "${AMI_ID} \
+      --instance-type "${INST_TYPE}" \
       --security-group-ids ${SG_ID} \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       | jq
