7fa950071e08bd487eda8be837a8f9796ed26cd3
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-18 18:34:42 -0700

    ec2-instance base script

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,6 +10,12 @@ COMPONENT="$1"
 aws ec2 run-instances \
       --image-id ami-0bb6af715826253bf \
       --instance-type t2.micro \
+      --security-group-ids ${SG_ID} \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       | jq
 
+
+SG_ID=$(aws ec2 describe-security-groups \
+        --filters "Name=group-name,Values=allow-all-sgp" \
+          | jq '.SecurityGroups[].GroupId' \
+          | sed -e 's/"//g')
