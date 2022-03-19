af449ce80e1380b70d90840e4fd424ca29a8d56f
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-18 18:02:48 -0700

    ec2-instance base script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo -e '\e[31m Machine Name Required\e[0m'
+  exit 1
+fi
+
+COMPONENT="$1"
+
 aws ec2 run-instances \
       --image-id ami-0bb6af715826253bf \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=My_script_ec2}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" \
       | jq
 
