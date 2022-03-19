8a900d2ab2dafc72653caf96974738858d672fd1
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-18 17:47:00 -0700

    ec2-instance base script

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,5 +4,5 @@ aws ec2 run-instances \
       --image-id ami-0bb6af715826253bf \
       --instance-type t2.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=My_script_ec2}]" \
-      | :jq
+      | jq
 
