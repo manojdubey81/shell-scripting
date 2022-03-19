9026907387a8dcdc9c1ba70e9bece3bea1da32e4
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-18 23:16:55 -0700

    ec2-instance base script

--- create-ec2.sh
+++ create-ec2.sh
@@ -22,9 +22,16 @@ SG_ID=$(aws ec2 describe-security-groups \
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" \
         | jq '.Images[].ImageId' | sed -e 's/"//g')
 
+VPC_ID=$(aws ec2 describe-vpcs | jq '.Vpcs[].VpcId' | sed -e 's/"//g')
+
+
 aws ec2 run-instances \
       --image-id "${AMI_ID}" \
       --instance-type "${INST_TYPE}" \
       --security-group-ids "${SG_ID}" \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       | jq
+
+PUB_HOST_ZONE=$(aws route53 create-hosted-zone --name roboshop.com --caller-reference "$(date)")
+PVT_HOST_ZONE=$(aws route53 create-hosted-zone --name roboshop.internal --vpc VPCRegion="us-east-1",VPCId=${VPC_ID} --caller-reference "$(date)")
+
