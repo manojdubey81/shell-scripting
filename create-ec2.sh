0038c8d019c292e3fddaebbc064e64d602784f47
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-23 15:07:45 -0700

    updated create-ec2 script

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,10 +2,19 @@
 
 if [ -z "$1" ]; then
   echo -e '\e[32mPlease Select Machine Name:\e[0m'
-  echo -e '\e[33m1) frontend\t\t2) catalogue\t\t 12) all\e[0m'
+  echo -e '\e[33m1) frontend\t\t2) mongodb\t\t 3) catalogue\e[0m'
+  echo -e '\e[33m4) redis\t\t5) user\t\t 6) cart\e[0m'
+  echo -e '\e[33m7) mysql\t\t8) shipping\t\t 9) rebbitmq\e[0m'
+  echo -e '\e[33m10) payment\t\t11) dispatch\t\t 12) all\e[0m'
+  read -p "Enter your choice : " choice
+  if [ "${choice}" == "1" ]; then
+    sed -i -e 's/$choice/frontend/g' > name
   exit 1
 fi
 
+echo "your select machine is: " $name
+
+#catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch
 if [ -z "$2" ]; then
   echo -e '\e[34mProvide Instances Type like t2.micro etc..\e[0m'
   exit 2
