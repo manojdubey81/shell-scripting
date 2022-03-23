98d84b2ba4bd3ba45f4dcc540ab6418a0c693ffe
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-03-23 15:22:57 -0700

    updated create-ec2 script

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,19 +2,9 @@
 
 if [ -z "$1" ]; then
   echo -e '\e[32mPlease Select Machine Name:\e[0m'
-  echo -e '\e[33m1) frontend\t\t2) mongodb\t\t 3) catalogue\e[0m'
-  echo -e '\e[33m4) redis\t\t5) user\t\t 6) cart\e[0m'
-  echo -e '\e[33m7) mysql\t\t8) shipping\t\t 9) rebbitmq\e[0m'
-  echo -e '\e[33m10) payment\t\t11) dispatch\t\t 12) all\e[0m'
-  read -p "Enter your choice : " choice
-  if [ "${choice}" == "1" ]; then
-    sed -i -e 's/$choice/frontend/g' > name
   exit 1
 fi
 
-echo "your select machine is: " $name
-
-#catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch
 if [ -z "$2" ]; then
   echo -e '\e[34mProvide Instances Type like t2.micro etc..\e[0m'
   exit 2
@@ -24,10 +14,24 @@ fi
 COMPONENT="$1"
 INST_TYPE="$2"
 
+PRIVATE_IP=$(aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=${COMPONENT}" \
+        --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
+if [ ! -z "${PRIVATE_IP}" ]; then
+    echo -e  "\e[31m Instance ${COMPONENT} is already exists, Hence not creating\e[0m"
+fi
+
+
 SG_ID=$(aws ec2 describe-security-groups \
-        --filters Name=group-name,Values=allow-all-sgp \
-          | jq '.SecurityGroups[].GroupId' \
-          | sed -e 's/"//g')
+          --filters Name=group-name,Values=allow-all-sgp \
+            | jq '.SecurityGroups[].GroupId' \
+            | sed -e 's/"//g')
+
+if [ -z "{SG_ID}"] ; then
+    echo -e "\e[1;33m Security Group allow-all-ports does not exist"
+    exit 3
+fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" \
         | jq '.Images[].ImageId' | sed -e 's/"//g')
